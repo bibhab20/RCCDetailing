739687b2d23f3eff8a00b69e3e1517793bd0d382
bibhab20 <pattnayak.bibhab@gmail.com>
2021-09-21 10:37:51 -0700

    Main class updated

--- RCCMain/src/View/Main.java
+++ RCCMain/src/View/Main.java
@@ -1,7 +1,31 @@
 package View;
 
+import Parsing.Parser;
+import StructuralModel.Beam;
+import StructuralModel.Structure;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Myfiles\\Tech Hobby\\NitinFolder\\nitinfile4.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+        StringBuilder sb = new StringBuilder();
+        while ((st = br.readLine()) != null)
+            sb.append(st);
+        String text = sb.toString();
+
+        //Nitin' code starts here
+        Parser parser = new Parser();
+        Structure structure = (Structure) parser.getStructure(text);
+        List<Beam> beams = structure.getBeams();
+        Beam firstBeam = beams.get(0);
 
     }
 }
